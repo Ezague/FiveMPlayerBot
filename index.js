613fbc0f650097c26a72e88f90ef39fec93f8c4d
Sh4dow <78806525+Sh4dow8080@users.noreply.github.com>
2022-02-24 18:25:04 +0100

    Cleaned up

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const client = new Client({ intents: ['GUILD_PRESENCES'] })
 const config = require('./config.json');
 const request = require('request');
 
-//Bliver kørt hvis botten starter korrekt
 client.on('ready', () => {
     console.log(`Loggede ind som ${client.user.tag}!`);
     function updatePlayerCount() {
@@ -12,11 +11,15 @@ client.on('ready', () => {
             url: `https://servers-frontend.fivem.net/api/servers/single/${config.cfxip}`,
             json: true,
             headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0' }
-        }, (err, res, data) => {
+        }, (err, res, _data) => {
             if (err) {
                 console.log('Error:', err);
-            } else if (res.statusCode !== 200) {
-                //server offline
+                return
+            }
+
+            const data = _data['Data'];
+
+            if (res.statusCode !== 200 || data?.clients == undefined) {
                 client.user.setPresence({
                     status: config.activity.status,
                     activities: [{
@@ -24,34 +27,23 @@ client.on('ready', () => {
                         type: config.activity.type
                     }]
                 })
-            } else {
-                //Server Online
-                const spillere = data['Data']['clients'];
-                if (spillere !== undefined) {
-                    //Antal spillere i alt
-                    const maxPlayers = data['Data']['sv_maxclients'];
-                    client.user.setPresence({
-                        status: config.activity.status,
-                        activities: [{
-                            name: `${spillere}/${maxPlayers} online`,
-                            type: config.activity.type
-                        }]
-                    })
-                } else {
-                    //Antal spillere ikke fundet
-                    client.user.setPresence({
-                        status: config.activity.status,
-                        activities: [{
-                            name: 'Server Offline',
-                            type: config.activity.type
-                        }]
-                    })
-                }
+                return
             }
+
+            const spillere = data?.clients;
+            const maxPlayers = data?.sv_maxclients;
+
+            client.user.setPresence({
+                status: config.activity.status,
+                activities: [{
+                    name: `${spillere}/${maxPlayers} online`,
+                    type: config.activity.type
+                }]
+            })
         });
     }
 
-    //Update status hver 5 sekunder
+    // Opdater bottens status hver 5 sekunder
     setInterval(updatePlayerCount, 5000);
 });
 
